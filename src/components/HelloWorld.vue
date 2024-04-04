9da3c10c720ef1fc3c8480b49c3de4a06fc64627
Richard Frey Reyes <rr55341@bose.com>
2024-04-04 08:30:25 +0800

    change vue version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,112 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
+<!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
   }
-})
-</script>
-
+  ```
+-->
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div class="bg-white">
+    <div
+      class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8"
+    >
+      <h2 class="sr-only">Products</h2>
+
+      <div
+        class="
+          grid grid-cols-1
+          gap-x-6 gap-y-10
+          sm:grid-cols-2
+          lg:grid-cols-3
+          xl:grid-cols-4 xl:gap-x-8
+        "
+      >
+        <a
+          v-for="product in products"
+          :key="product.id"
+          :href="product.href"
+          class="group"
+        >
+          <div
+            class="
+              aspect-h-1 aspect-w-1
+              w-full
+              overflow-hidden
+              rounded-lg
+              bg-gray-200
+              xl:aspect-h-8 xl:aspect-w-7
+            "
+          >
+            <img
+              :src="product.imageSrc"
+              :alt="product.imageAlt"
+              class="
+                h-full
+                w-full
+                object-cover object-center
+                group-hover:opacity-75
+              "
+            />
+          </div>
+          <h3 class="mt-4 text-sm text-gray-700">{{ product.name }}</h3>
+          <p class="mt-1 text-lg font-medium text-gray-900">
+            {{ product.price }}
+          </p>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
+<script setup>
+const products = [
+  {
+    id: 1,
+    name: 'Earthen Bottle',
+    href: '#',
+    price: '$48',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+    imageAlt:
+      'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+  },
+  {
+    id: 2,
+    name: 'Nomad Tumbler',
+    href: '#',
+    price: '$35',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
+    imageAlt:
+      'Olive drab green insulated bottle with flared screw lid and flat top.',
+  },
+  {
+    id: 3,
+    name: 'Focus Paper Refill',
+    href: '#',
+    price: '$89',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+    imageAlt:
+      'Person using a pen to cross a task off a productivity paper card.',
+  },
+  {
+    id: 4,
+    name: 'Machined Mechanical Pencil',
+    href: '#',
+    price: '$35',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+    imageAlt:
+      'Hand holding black machined steel mechanical pencil with brass tip and top.',
+  },
+  // More products...
+];
+</script>
